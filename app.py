fb1f984080ce77af6f40705ff818005bc6ae551d
daferali20 <31751340+daferali20@users.noreply.github.com>
2025-06-02 15:25:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 import pandas as pd
 # تحميل ملف CSS
 def load_css():
-    with open("./streamlit/style.css") as f:
+    with open("./style.css") as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 load_css()
